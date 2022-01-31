857f564ecf783cf345e4bc3b8255ff2f6f98a6e5
ArtyomKozyrev8 <kozirev8@gmail.com>
2022-01-31 11:03:13 +0300

    implemented linked list

---
+++ Pr_Solv_Algo_DataStr_UsingPython/ch4_basic_data_str/linked_list.py
@@ -0,0 +1,160 @@
+from typing import Union, Any
+
+
+class Node:
+    def __init__(self, val: Any):
+        self.val = val
+        self._next = None
+
+    @property
+    def next(self) -> Union["Node", None]:
+        return self._next
+
+    @next.setter
+    def next(self, next_node: Union["Node", None]) -> None:
+        self._next = next_node
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.val})"
+
+
+class List:
+    def __init__(self):
+        self._head = None
+        self._len = 0
+
+    def __repr__(self):
+        cur = self._head
+        items = []
+        while cur is not None:
+            items.append(cur.val)
+            cur = cur.next
+
+        return f"List({', '.join(map(str, items))})"
+
+    def add(self, item: Node) -> None:
+        temp = self._head
+        item.next = temp
+        self._head = item
+        self._len += 1
+
+    def remove(self, item: Node) -> None:
+        if self.is_empty():
+            raise IndexError(f"Can't remove from empty list")
+
+        cur = self._head
+        prev = None
+        while cur is not None:
+            if cur.val == item.val:
+                if prev is None:
+                    self._head = cur.next
+                else:
+                    prev.next = cur.next
+                self._len -= 1
+                return
+            prev = cur
+            cur = cur.next
+
+
+        raise ValueError(f"{item} is not in the List")
+
+    def search(self, item: Node) -> bool:
+        cur = self._head
+        while cur is not None:
+            if cur.val == item.val:
+                return True
+            cur = cur.next
+
+        return False
+
+    def is_empty(self) -> bool:
+        if self._head is None:
+            return True
+        return False
+
+    def size(self) -> int:
+        return self._len
+
+    def append(self, item: Node) -> None:
+        cur = self._head
+        while cur is not None:
+            temp = cur.next
+            if temp is None:
+                break
+            cur = cur.next
+        cur.next = item
+        self._len += 1
+
+    def index(self, item: Node) -> int:
+        cur = self._head
+        index = 0
+        while cur is not None:
+            if cur.val == item.val:
+                return index
+            else:
+                cur = cur.next
+                index += 1
+        raise ValueError(f"{item} is not in the list")
+
+    def insert(self, pos: int, item: Node) -> None:
+        cur = self._head
+        prev = None
+        cur_pos = 0
+        while cur is not None:
+            if cur_pos == pos:
+                if prev is None:
+                    self._head = item
+                    self._head.next = cur
+                    self._len += 1
+                    return
+                else:
+                    prev.next = item
+                    item.next = cur
+                    self._len += 1
+                    return
+            cur_pos += 1
+            prev = cur
+            cur = cur.next
+
+        if prev:
+            prev.next = item
+            item.next = cur
+        else:
+            self._head = item
+        self._len += 1
+
+    def pop(self, pos: int = None) -> Node:
+        if self._len == 0:
+            raise IndexError("Cant remove from empty List")
+
+        cur = self._head
+        prev = None
+
+        if pos is None:
+            while cur.next is not None:
+                prev = cur
+                cur = cur.next
+
+            if prev is None:
+                self._head = None
+            else:
+                prev.next = None
+
+            self._len -= 1
+        else:
+            if self._len - 1 < pos:
+                raise IndexError(f"Can't remove item in position {pos}. List length is {self._len}")
+
+            index = 0
+            while cur is not None:
+                if index == pos:
+                    if prev is None:
+                        self._head = cur.next
+                        break
+                    else:
+                        prev.next = cur.next
+
+                prev = cur
+                cur = cur.next
+                index += 1
+            self._len -= 1
---
+++ tests_Pr_Solv_Algo_DataStr_UsingPython/test_ch4/test_linked_list.py
@@ -0,0 +1,85 @@
+import pytest
+
+from Pr_Solv_Algo_DataStr_UsingPython.ch4_basic_data_str.linked_list import List, Node
+
+
+def test_node():
+    x = Node(5)
+    assert x.next is None
+    assert str(x) == "Node(5)"
+    x.next = Node(10)
+    assert str(x.next) == "Node(10)"
+
+
+def test_list():
+    x = List()
+    assert str(x) == "List()"
+    assert x.is_empty() is True
+    assert x.size() == 0
+    x.add(Node(1))
+    x.add(Node(2))
+    x.add(Node(3))
+    assert str(x) == "List(3, 2, 1)"
+    assert x.is_empty() is False
+    assert x.size() == 3
+    x.append(Node(4))
+    x.append(Node(5))
+    assert str(x) == "List(3, 2, 1, 4, 5)"
+    assert x.is_empty() is False
+    assert x.size() == 5
+    assert x.search(Node(4)) is True
+    assert x.search(Node(6)) is False
+    assert x.index(Node(3)) == 0
+    assert x.index(Node(2)) == 1
+    assert x.index(Node(1)) == 2
+    assert x.index(Node(4)) == 3
+    assert x.index(Node(5)) == 4
+    with pytest.raises(Exception):
+        assert x.index(Node(7))
+    x.insert(0, Node(6))
+    assert str(x) == "List(6, 3, 2, 1, 4, 5)"
+    assert x.size() == 6
+    x.insert(3, Node(7))
+    assert str(x) == "List(6, 3, 2, 7, 1, 4, 5)"
+    assert x.size() == 7
+    x.insert(10, Node(8))
+    assert str(x) == "List(6, 3, 2, 7, 1, 4, 5, 8)"
+    assert x.size() == 8
+    y = List()
+    y.insert(10, Node(10))
+    assert str(y) == "List(10)"
+    assert y.size() == 1
+    y.pop()
+    assert str(y) == "List()"
+    assert y.size() == 0
+    assert y.is_empty() is True
+    with pytest.raises(Exception):
+        assert y.pop()
+    with pytest.raises(Exception):
+        assert y.remove(Node(9))
+    x.pop()
+    assert str(x) == "List(6, 3, 2, 7, 1, 4, 5)"
+    assert x.size() == 7
+    x.pop(0)
+    assert str(x) == "List(3, 2, 7, 1, 4, 5)"
+    assert x.size() == 6
+    with pytest.raises(Exception):
+        x.pop(10)
+    x.pop(3)
+    assert str(x) == "List(3, 2, 7, 4, 5)"
+    assert x.size() == 5
+    x.pop(4)
+    assert str(x) == "List(3, 2, 7, 4)"
+    assert x.size() == 4
+    with pytest.raises(Exception):
+        x.remove(Node(10))
+    x.remove(Node(7))
+    assert str(x) == "List(3, 2, 4)"
+    assert x.size() == 3
+    x.remove(Node(3))
+    assert str(x) == "List(2, 4)"
+    assert x.size() == 2
+    x.remove(Node(4))
+    assert str(x) == "List(2)"
+    assert x.size() == 1
+
